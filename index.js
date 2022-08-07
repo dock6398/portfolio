14c9351f6a51f221fec3b631a83b34c40c20984f
dock6398 <101781629+dock6398@users.noreply.github.com>
2022-08-07 20:17:16 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const textloop = new Typewriter('#looptext', {
+        loop : true,
+        deleteSpeed : 50,
+        delay : 50
+    });
+
+    textloop.typeString('DONGJAE like to watch videos')
+        .pauseFor(1000)
+        .deleteChars(21)
+        .typeString(' like ice Americano')
+        .pauseFor(1000)
+        .deleteChars(19)
+        .typeString(' love Chocolate')
+        .pauseFor(1000)
+        .deleteChars(15)
+        .typeString(' have six really close friends')
+        .deleteChars(30)
+        .start();
+
+    /* arrow 효과 */
+    const box = document.querySelector('#arrow')
+
+    box.animate({
+        transform: [
+            'translateY(0)',
+            'translateY(50px) ',
+        ]
+    },
+    {
+        duration : 2000,// 시간
+        iterations: Infinity,// 반복 횟수
+        direction: 'normal', // 방향
+        easing: 'ease',// 움직임 형태
+        fill : 'forwards'
+    })
+
+    const bg1 = document.querySelector('#project1 h2')
+    const bg2 = document.querySelector('#project2 h2')
+    const bg3 = document.querySelector('#project3 h2')
+    const bg4 = document.querySelector('#project4 h2')
+    const body = document.querySelector('body')
+
+    bg1.addEventListener('mouseover', () => {
+        body.style.backgroundColor = 'pink'
+    })
+    bg2.addEventListener('mouseover', () => {
+        body.style.backgroundColor = 'gold'
+    })
+    bg3.addEventListener('mouseover', () => {
+        body.style.backgroundColor = 'lightblue'
+    })
+    bg4.addEventListener('mouseover', () => {
+        body.style.backgroundColor = 'tomato'
+    })
+});
